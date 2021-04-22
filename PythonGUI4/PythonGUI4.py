e59a4e44cd2333fe1818a60a933e444feb85451c
Will Hammond <HAM202002036@longroad.ac.uk>
2021-04-22 11:56:34 +0100

    Flashcard completed

--- PythonGUI4/PythonGUI4.py
+++ PythonGUI4/PythonGUI4.py
@@ -2,34 +2,46 @@ from tkinter import *
 
 window = Tk()
 
+my_dictionary = {"CPU": "Central processing unit is the brains of the computer, helps process everything", "RAM": "Random Access Memory - helps multiple things run at once", }
+
 window.title("Flashcards")
 
 label1 = Label(window, text="Enter: ")
 label1.grid(row=1, column=0, sticky=E)
 
-label2 = Label(window, text="The CPU is responsible for...")
+label2 = Label(window, text="")
 
 entry1 = Entry(window, width=20, bg="light blue")
 entry1.grid(row=2, column=0, sticky=E)
 
-def button_click():
-    typed_text = entry1.get()
+def delet():
+    output_text.delete(0,0, END)
 
-    print(typed_text)
 
-button1 = Button(window,text="Motherboards", width=10, command=button_click)
-button1.grid(row=1, column=2, sticky=E)
+def cpu_button_click():
+    output_text.delete(0.0, END)
+    Screentxt = (my_dictionary["CPU"])
+    output_text.insert(END, Screentxt)
+   
+    
 
-button2 = Button(window,text="Memory", width=10, command=button_click)
+def ram_button_click():
+    output_text.delete(0.0, END)
+    Screentxt = (my_dictionary["RAM"])
+    output_text.insert(END, Screentxt)
+    
+cpu_button1 = Button(window,text="CPU", width=10, command=cpu_button_click)
+cpu_button1.grid(row=1, column=2, sticky=E)
+
+button2 = Button(window,text="RAM", width=10, command=ram_button_click)
 button2.grid(row=2, column=2, sticky=E)
 
-button3 = Button(window,text="Storage Devices", width=10, command=button_click)
-button3.grid(row=3, column=2, sticky=E)
+
 
 output_text = Text(window, width=40, height=10, wrap=WORD, background = "blue")
 output_text.grid(row=4, column=0, columnspan=2, sticky = E)
 
-my_dictionary = ["Motherboards", "Storage Devices", "Memory]
+
 
 
 window.mainloop()
